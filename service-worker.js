6c09a5d948d3ca19daec55b0a1651f34e20c1dd7
Andrey Fomin <a48671@gmail.com>
2019-04-19 16:11:30 +0300

    build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffa819d4735a2f90a734e513e26c8f27.js"
+  "/precache-manifest.1bf95340ff5c00820d70f642ea9ee7c9.js"
 );
 
 workbox.clientsClaim();
